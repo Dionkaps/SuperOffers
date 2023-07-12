d477dac5767168949cad2649881bf70b099f1d87
Dionisis Konstantinos Kapsimalis <vpddkf@gmail.com>
2023-07-13 01:56:59 +0300

    updates

--- LoginPage/inputCheck.js
+++ LoginPage/inputCheck.js
@@ -1,5 +1,5 @@
 //Sinartisi elegxou olwn twn pediwn gia na ginei enable to koumpi sign up
-function inputCheckFunction(inputPass,inputFname,inputLname,inputEmail,inputUsername) {
+function inputCheckFunction(inputPass, inputFname, inputLname, inputEmail, inputUsername) {
   const signUpBtn = document.getElementById('signUpbtn');
   if (inputPass.value.length > 0 && inputFname.value.length > 0 && inputLname.value.length > 0 && inputEmail.value.length > 0 && inputUsername.value.length > 0) {
     signUpBtn.style.pointerEvents = 'auto';
@@ -11,14 +11,30 @@ function inputCheckFunction(inputPass,inputFname,inputLname,inputEmail,inputUser
 }
 
 //Sinartisi elegxou tou password
-function passCheck(inputPass, form) {
+function passCheck(inputEmail, inputPass, form) {
   form.action = 'signup.php';
   var passw = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  var x = 2;
 
-  if (inputPass.value.match(passw)) {
-    form.submit();
-  } else {
+  if (!inputPass.value.match(passw)) {
+    x = 0;
+  }
+
+  if (!inputEmail.value.match(emailRegex)) {
+    x = 1;
+  }
+
+  if (x == 0) {
     alert('Your password must contain at least 8 characters, one uppercase, one number, and one special character');
     event.preventDefault(); //Prevent form submission
+  }
+  else if (x == 1) {
+    alert('This is not a valid email');
+    event.preventDefault(); //Prevent form submission
+  }
+  else{
+    form.submit();
+    x = 2;
   }
 }
