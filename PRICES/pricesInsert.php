ddad1b777313de3aee624dc346951c25bc8050eb
Dionisis Konstantinos Kapsimalis <vpddkf@gmail.com>
2023-07-29 09:28:28 +0300

    Script for prices insertion + correct prices json

--- PRICES/pricesInsert.php
+++ PRICES/pricesInsert.php
@@ -8,39 +8,40 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$json = 'prices.json';
+$json = file_get_contents('prices.json');
 
 $data = json_decode($json, true);
 
-//Insert prices into the 'prices' table
 foreach ($data['data'] as $product) {
-    //Get the product details from the JSON
+    $productId = $product['id'];
     $productName = $product['name'];
     $productPrices = $product['prices'];
 
-    //Retrieve the product ID from the 'products' table
-    $productIdQuery = "SELECT id FROM products WHERE name = '$productName'";
-    $result = mysqli_query($connection, $productIdQuery);
+    //Escape the product name to avoid SQL syntax errors
+    $productName = mysqli_real_escape_string($conn, $productName);
 
-    //Check if the product exists in the 'products' table
+    //Retrieve the product ID from the products table
+    $productIdQuery = "SELECT id FROM products WHERE name = '$productName'";
+    $result = mysqli_query($conn, $productIdQuery);
+
+    //Check if the product exists in the products table
     if (mysqli_num_rows($result) > 0) {
         //Product exists, fetch the product ID
         $row = mysqli_fetch_assoc($result);
         $productId = $row['id'];
 
-        //Insert prices into the 'prices' table
+        //Insert prices into the prices table
         foreach ($productPrices as $price) {
             $date = $price['date'];
             $priceValue = $price['price'];
 
-            //Insert price along with the corresponding product ID into the 'prices' table
-            $insertPriceQuery = "INSERT INTO prices (product_id, date, price) VALUES ($productId, '$date', $priceValue)";
-            mysqli_query($connection, $insertPriceQuery);
+            $insertPriceQuery = "INSERT INTO prices (pricing_id, product_id, date, price) VALUES ($productId, $productId, '$date', $priceValue)";
+            mysqli_query($conn, $insertPriceQuery);
         }
     } else {
-        //Handle the case where the product does not exist in the 'products' table
-        //You can choose to skip inserting prices or handle it as per your requirements
+        //Handle the case where the product does not exist in the products table
         echo "Product '$productName' does not exist in the 'products' table. Skipping prices insertion.<br>";
     }
 }
+
 $conn->close();
